172efdba5d9f0a57f587c1a4f1ba4e62becf7f78
Charugoyal3 <charugoyal180@gmail.com>
2020-09-15 15:10:42 +0530

    added

---
+++ src/context/GlobalState.js
@@ -0,0 +1,37 @@
+import React, {createContext, useReducer} from 'react';
+import AppReducer from './AppReducer';
+
+const initialState = {
+    transactions:[]
+}
+
+//Create context
+export const GlobalContext =  createContext(initialState);
+
+//Provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+    
+    //Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type:'Delete_Transaction',
+            payload:id
+        })
+    }
+
+    function addTransaction(transaction) {
+        dispatch({
+            type:'Add_Transaction',
+            payload:transaction
+        });
+    }
+
+    return (<GlobalContext.Provider value={{
+        transactions:state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>
+        {children}
+    </GlobalContext.Provider>);
+}
